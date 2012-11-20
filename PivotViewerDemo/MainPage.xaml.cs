422fb4632d0e9b78b394afd14851040d81a75b7c
Steven Hollidge <stevenhollidge@hotmail.com>
2012-11-20 22:23:00 +0100

    tidy up

--- PivotViewerDemo/MainPage.xaml.cs
+++ PivotViewerDemo/MainPage.xaml.cs
@@ -17,41 +17,15 @@ namespace PivotViewerDemo
             InitializeComponent();
 
             this.pivotViewer.ItemsSource = Factory.GetData();
-            var output = new StringBuilder();
-            foreach (var i in Enumerable.Range(126, 206)) output.Append(i + ",");
-
-            //this.pivotViewer.View = this.pivotViewer.GraphView;
+            
+            this.pivotViewer.View = this.pivotViewer.GraphView;
             //this.pivotViewer.Loaded += pivotViewer_Loaded;
         }
 
         void pivotViewer_Loaded(object sender, RoutedEventArgs e)
         {
             //this.pivotViewer.Filter = "CouponToday=GE.5_LE.10";
-            //this.pivotViewer.SelectionChanged += pivotViewer_SelectionChanged;
-            //var filterRoot = (Grid) this.FindName("FilterPaneRoot");
-            //var dropShadow = (Rectangle)filterRoot.Children[0];
-            //filterRoot.Children.Remove(dropShadow);
-
-            //var filterRoot = this.FindName("FilterPaneRoot");
-            //VisualTreeHelper.(pivotViewer);
-            //var dropShadow = (UIElement)VisualTreeHelper.GetChild(filterRoot, 0);
-            //filterRoot.Children.Remove(dropShadow);
-
-            //(UIElement) VisualTreeHelper.GetChild(this.pivotViewer);
         }
-
-        //void pivotViewer_SelectionChanged(object sender, EventArgs e)
-        //{
-        //    string filter = pivotViewer.Filter;
-        //    string inScopeItems = pivotViewer.InScopeItems.Count.ToString();
-        //    string viewId = pivotViewer.View.Id;
-
-        //    MessageBox.Show(string.Format("Filter: {1}{0}InScopeItems.Count: {2}{0}View: {3}",
-        //                                    Environment.NewLine,
-        //                                    filter,
-        //                                    inScopeItems,
-        //                                    viewId));
-        //}
 
         private void GetCommands(object sender, PivotViewerCommandsRequestedEventArgs e)
         {
